8b45ebd6674c8983297d3ddc9ffa743f7af38371
Prashant168 <prashant1931210@akgec.ac.in>
2022-08-21 12:24:11 +0530

    first

---
+++ src/data.js
@@ -0,0 +1,76 @@
+export const sliderItems = [
+    {
+        id: 1,
+        img: "https://cdn.create.vista.com/api/media/medium/361417462/stock-photo-beautiful-stylish-couple-posing-summer?token=",
+        title: "SUMMER SALE",
+        desc: "DONT'T COMPROMISE ON STYLE! GET FLAT 50% OFF FOR NEW ARRIVALS.",
+        bg: "f5fafd",
+    },
+    {
+        id: 2,
+        img: "https://i.pinimg.com/236x/a7/42/86/a7428657980197df8e1b4fae90a65e4b--fashion-design-fashion-tips.jpg",
+        title: "AUTUMN COLLECTION",
+        desc: "DONT'T COMPROMISE ON STYLE! GET FLAT 50% OFF FOR NEW ARRIVALS.",
+        bg: "fcf1ed",   
+    },
+    {
+        id: 3,
+        img: "https://s3.envato.com/files/342217621/200813-27.jpg",
+        title: "LOUNGEWEAR LOVE",
+        desc: "DONT'T COMPROMISE ON STYLE! GET FLAT 50% OFF FOR NEW ARRIVALS.",
+        bg: "fbf0f4",   
+    },
+];
+
+export const categories = [
+    {
+        id: 1,
+        img: "https://cdn.shopify.com/s/files/1/0597/8805/files/Luca-Faloni_Steel-Blue-Siena_Collection-Banner_Desktop_Made-in-Italy.jpg?v=1623325000",
+        title: "SHIRT STYLE",
+    },
+    {
+        id: 2,
+        img: "https://www.bridalguide.com/sites/default/files/Gown-images/dessy_loungewear/TkzWInDA-sized.jpg",
+        title: "LOUNGEWEAR LOVE",
+    },
+    {
+        id: 3,
+        img: "https://www.atlasformen.co.uk/repository/sites/20/en-GB/Inspiration/Buying%20guides/summer%20jackets/mens-denim-jacket.jpg",
+        title: "LIGHT JACKETS",
+    },
+];
+
+export const popularProducts = [
+    {
+        id:1,
+        img: "https://lp2.hm.com/hmgoepprod?set=quality%5B79%5D%2Csource%5B%2F06%2Fb9%2F06b9866e36d0f9095539c132f05168e206b09074.jpg%5D%2Corigin%5Bdam%5D%2Ccategory%5Bkids_boy8y_topstshirts%5D%2Ctype%5BDESCRIPTIVESTILLLIFE%5D%2Cres%5Bm%5D%2Chmver%5B2%5D&call=url[file:/product/main]",
+    },
+    {
+        id:2,
+        img: "https://m.media-amazon.com/images/I/61x86ZfM8JL._UL1000_.jpg",
+    },
+    {
+        id:3,
+        img: "https://sslimages.shoppersstop.com/sys-master/images/had/h09/16117914796062/S21711BSHIRT15_WHITE_alt1.jpg_230Wx334H",
+    },
+    {
+        id:4,
+        img: "https://m.hng.io/catalog/product/8/9/895023_green_and_other_1.jpg?io=PLP&dpr=2.625",
+    },
+    {
+        id:5,
+        img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR4HPCKgxR70FKJFq98o17xNZWzVUxlGEvTgA&usqp=CAU",
+    },
+    {
+        id:6,
+        img: "https://assets.ajio.com/medias/sys_master/root/20211214/JGIl/61b7ad51aeb26901100a3ae2/-473Wx593H-441221895-blue-MODEL.jpg",
+    },
+    {
+        id:7,
+        img: "https://5.imimg.com/data5/RG/GV/MY-944516/men-27s-casual-jacket-500x500.jpg",
+    },
+    {
+        id:8,
+        img: "https://media-cldnry.s-nbcnews.com/image/upload/t_fit-720w,f_auto,q_auto:best/newscms/2020_51/3436191/4166jf0x1gl-5fda4e8a0a921.jpg",
+    },
+];
